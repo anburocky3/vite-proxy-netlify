d693d340891d1a9af6507f01fafa1c10ea30e6b6
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-10 17:26:00 +0530

    with axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,43 @@
-// Write your javascript code here
+import axios from "axios";
+
+const ipAddressPlaceholder = document.querySelector("#ipAddress");
+// Get the ip address from this url: https://ipv4.icanhazip.com/
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     if (this.status === 200) {
+//       ipAddressPlaceholder.textContent = xhr.responseText;
+//     } else {
+//       ipAddressPlaceholder.innerHTML =
+//         "<div class='alert-danger'>COULDN'T GET YOUR IP. <a href='/Get-My-IP-Web-App/' class='retry-btn'>Retry</a></div>";
+//     }
+//   }
+// };
+// xhr.open(
+//   "GET",
+//   // "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
+//   "/ip"
+// );
+// xhr.send();
+
+const URL = "https://ipv4.icanhazip.com/";
+// const URL = "/ip"
+
+async function logMovies() {
+  const response = await fetch(URL);
+  const movies = await response.text();
+  console.log(movies);
+}
+
+const searchFromApi = async (query) => {
+  const result = await axios.get(URL, {
+    params: query,
+  });
+
+  console.log(result.data);
+};
+
+logMovies();
+searchFromApi();
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
-  //   build: {
-  //     rollupOptions: {
-  //       input: {
-  //         // main: resolve(__dirname, "index.html"),
-  //       },
-  //     },
+  // base: "/Get-My-IP-Web-App/",
+  // server: {
+  // cors: true,
+  // proxy: {
+  //   "/ip": {
+  //     target: "https://ipv4.icanhazip.com/",
+  //     changeOrigin: true,
+  //     secure: false,
   //   },
+  // },
+  // },
 });
