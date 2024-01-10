8583bd35a4b6aa8fc7443896fc84d7e08fe079b2
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-10 18:37:29 +0530

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
-export default defineConfig({
-  // base: "/Get-My-IP-Web-App/",
-  // server: {
-  // cors: true,
-  // proxy: {
-  //   "/ip": {
-  //     target: "https://ipv4.icanhazip.com/",
-  //     changeOrigin: true,
-  //     secure: false,
-  //   },
-  // },
-  // },
-});
+export default defineConfig({});
