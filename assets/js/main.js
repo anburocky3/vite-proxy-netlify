7c4f3abece31472d78f35a71276d7f51e47db921
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-10 18:31:08 +0530

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,32 +3,34 @@ import axios from "axios";
 const ipAddressPlaceholder = document.querySelector("#ipAddress");
 // Get the ip address from this url: https://ipv4.icanhazip.com/
 
-// let xhr = new XMLHttpRequest();
-
-// xhr.onreadystatechange = function () {
-//   if (this.readyState === 4) {
-//     if (this.status === 200) {
-//       ipAddressPlaceholder.textContent = xhr.responseText;
-//     } else {
-//       ipAddressPlaceholder.innerHTML =
-//         "<div class='alert-danger'>COULDN'T GET YOUR IP. <a href='/Get-My-IP-Web-App/' class='retry-btn'>Retry</a></div>";
-//     }
-//   }
-// };
-// xhr.open(
-//   "GET",
-//   // "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
-//   "/ip"
-// );
-// xhr.send();
-
-const URL = "https://ipv4.icanhazip.com/";
 // const URL = "/ip"
+const URL = "https://ipv4.icanhazip.com/";
 
-async function logMovies() {
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    if (this.status === 200) {
+      ipAddressPlaceholder.textContent = xhr.responseText;
+
+      console.log("xhr: ", xhr.responseText);
+    } else {
+      ipAddressPlaceholder.innerHTML =
+        "<div class='alert-danger'>COULDN'T GET YOUR IP. <a href='/Get-My-IP-Web-App/' class='retry-btn'>Retry</a></div>";
+    }
+  }
+};
+xhr.open(
+  "GET",
+  // "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
+  URL
+);
+xhr.send();
+
+async function logIP() {
   const response = await fetch(URL);
-  const movies = await response.text();
-  console.log(movies);
+  const ipAddress = await response.text();
+  console.log("fetchAPI", ipAddress);
 }
 
 const searchFromApi = async (query) => {
@@ -36,8 +38,9 @@ const searchFromApi = async (query) => {
     params: query,
   });
 
-  console.log(result.data);
+  ipAddressPlaceholder.textContent = result.data;
+  console.log("axios", result.data);
 };
 
-logMovies();
+logIP();
 searchFromApi();
